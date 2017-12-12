18e5f96d296110fc000da752f12d4ea4d42a0832
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2017-12-12 15:33:43 +0000

    TSV-767 Added integration tests to verify that GET to the Personal Details page fails for an unknown validationId.

--- it/uk/gov/hmrc/personaldetailsvalidationfrontend/specs/BaseIntegrationSpec.scala
+++ it/uk/gov/hmrc/personaldetailsvalidationfrontend/specs/BaseIntegrationSpec.scala
@@ -1,6 +1,6 @@
 package uk.gov.hmrc.personaldetailsvalidationfrontend.specs
 
-import org.scalatest.WordSpec
+import org.scalatest.{FeatureSpec, GivenWhenThen}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
@@ -8,8 +8,10 @@ import play.api.inject.guice.GuiceApplicationBuilder
 import uk.gov.hmrc.personaldetailsvalidationfrontend.support.{ImplicitWebDriverSugar, NavigationSugar, SingletonDriver}
 
 trait BaseIntegrationSpec
-  extends WordSpec with GuiceOneServerPerSuite with ScalaFutures
-    with IntegrationPatience with ImplicitWebDriverSugar with NavigationSugar {
+  extends FeatureSpec with GivenWhenThen
+    with GuiceOneServerPerSuite
+    with ScalaFutures with IntegrationPatience
+    with ImplicitWebDriverSugar with NavigationSugar {
 
   protected def additionalConfiguration = Map.empty[String, Any]
 
@@ -19,11 +21,9 @@ trait BaseIntegrationSpec
 
   protected val baseUrl = s"http://localhost:$port/personal-details-validation"
 
-  protected def goTo(url: String) = super.goTo(s"$baseUrl$url")
+  protected def goTo(url: String): Unit = super.goTo(s"$baseUrl$url")
 
   sys addShutdownHook {
     SingletonDriver.closeInstance()
   }
 }
-
-
--- it/uk/gov/hmrc/personaldetailsvalidationfrontend/specs/PersonalDetailsValidationStartISpec.scala
+++ it/uk/gov/hmrc/personaldetailsvalidationfrontend/specs/PersonalDetailsValidationStartISpec.scala
@@ -1,22 +1,46 @@
 package uk.gov.hmrc.personaldetailsvalidationfrontend.specs
 
+import java.util.UUID
+
 import uk.gov.hmrc.personaldetailsvalidationfrontend.pages.{ErrorPage, PersonalDetailsPage}
 
 class PersonalDetailsValidationStartISpec extends BaseIntegrationSpec {
 
-  "GET /personal-details-validation/start" should {
-    "redirect to personal details page" in {
+  feature("Personal Details validation journey") {
+
+    scenario("Personal Details validation journey started with a valid completionUrl") {
+
+      When("I navigate to /personal-details-validation/start with a valid completionUrl")
       goTo("/start?completionUrl=/foobar")
+
+      Then("I should get redirected to the Personal Details page")
       on(PersonalDetailsPage)
     }
 
-    "render error page if completionUrl is not provided" in {
-      goTo("/start")
+    scenario("Personal Details validation page accessed without a valid journeyId") {
+
+      When("I navigate to /personal-details-validation/personal-details with a non-existing journeyId")
+      goTo(s"/personal-details?journeyId=${UUID.randomUUID()}")
+
+      Then("I should see the error page")
       on(ErrorPage)
     }
 
-    "render error page if completionUrl is not relative" in {
+    scenario("Personal Details validation journey started without a completionUrl parameter") {
+
+      When("I navigate to /personal-details-validation/start without a completionUrl parameter")
+      goTo("/start")
+
+      Then("I should see the error page")
+      on(ErrorPage)
+    }
+
+    scenario("Personal Details validation journey started with an invalid completionUrl") {
+
+      When("I navigate to /personal-details-validation/start with an invalid completionUrl")
       goTo("/start?completionUrl=http://foobar")
+
+      Then("I should see the error page")
       on(ErrorPage)
     }
   }
