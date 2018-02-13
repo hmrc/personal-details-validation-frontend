6d683c4df6a1449207087ccebc616a6e635ba491
somme <sommes.email@gmail.com>
2018-02-13 14:09:13 +0000

    [TSV-936][SS] added styles and js for accessibility issue fixes

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+    //Accessibility
+    var errorSummary =  $('#error-summary-display'),
+    $input = $('input:text')
+    //Error summary focus
+    if (errorSummary){ errorSummary.focus() }
+    $input.each( function(){
+        if($(this).closest('label').hasClass('form-field--error')){
+            $(this).attr('aria-invalid', true)
+        }else{
+            $(this).attr('aria-invalid', false)
+        }
+    });
+    //Add aria-hidden to hidden inputs
+    $('[type="hidden"]').attr("aria-hidden", true)
+
+    var showHideContent = new GOVUK.ShowHideContent()
+    showHideContent.init()
+});
