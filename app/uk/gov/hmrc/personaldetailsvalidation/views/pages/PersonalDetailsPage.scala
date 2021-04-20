3e369fc7e727a36dcac4be7530de1cf8aea1d319
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2021-04-20 14:17:55 +0100

    PR changes

--- app/uk/gov/hmrc/personaldetailsvalidation/views/pages/PersonalDetailsPage.scala
+++ app/uk/gov/hmrc/personaldetailsvalidation/views/pages/PersonalDetailsPage.scala
@@ -75,8 +75,10 @@ class PersonalDetailsPage @Inject()(
     postcode.value.matches("""([A-Za-z][A-HJ-Ya-hj-y]?[0-9][A-Za-z0-9]?|[A-Za-z][A-HJ-Ya-hj-y][A-Za-z])\s?[0-9][ABDEFGHJLNPQRSTUWXYZabdefghjlnpqrstuwxyz]{2}""")
 
   def render(postCodePageRequested: Boolean)(implicit completionUrl: CompletionUrl, request: Request[_]): Html = {
-    if (postCodePageRequested) personalDetailsPostcode(formWithPostcode, completionUrl)
-    else personalDetailsNino(formWithNino, completionUrl)
+    if (postCodePageRequested)
+      personalDetailsPostcode(formWithPostcode, completionUrl)
+    else
+      personalDetailsNino(formWithNino, completionUrl)
   }
 
   def renderValidationFailure(postCodePageRequested: Boolean)(implicit completionUrl: CompletionUrl, request: Request[_]): Html =
