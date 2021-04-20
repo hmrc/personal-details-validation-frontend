2e6377c1a7a4df2880d4f09cd977a2a98e40bda6
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2021-04-20 11:25:06 +0100

    Kill the it tests as Play 27 doesn't like them and they are covered elsewhere

--- it/uk/gov/hmrc/personaldetailsvalidation/pages/ErrorPage.scala
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmrc.personaldetailsvalidation.pages
-
-import uk.gov.hmrc.personaldetailsvalidation.support.WebPage
-
-case class ErrorPage(url: String = "") extends WebPage {
-
-  def verifyThisPageDisplayed(): Unit =
-    pageTitle shouldBe "Sorry, we are experiencing technical difficulties"
-}
--- it/uk/gov/hmrc/personaldetailsvalidation/support/ImplicitWebDriverSugar.scala
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmrc.personaldetailsvalidation.support
-
-import org.openqa.selenium.WebDriver
-
-trait ImplicitWebDriverSugar {
-  implicit lazy val webDriver: WebDriver = SingletonDriver.driver
-}
