0cfb6db356d087e6fd9f0816e4955f71562672a0
Gaurav Sood <88654936+daglisood@users.noreply.github.com>
2026-03-13 13:09:59 +0000

    VER-4174: Upgrading dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,20 @@ import sbt.*
 
   def apply(): Seq[ModuleID] = compile ++ test
 
-  val bootstrapVersion = "10.4.0"
+  val bootstrapVersion = "10.7.0"
 
   val compile = Seq(
     "org.typelevel" %% "cats-core"                  % "2.13.0",
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"   %% "domain-play-30"             % "11.0.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.23.0"
+    "uk.gov.hmrc"   %% "domain-play-30"             % "13.0.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30" % "12.32.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.jsoup"               %  "jsoup"                  % "1.21.2",
+    "org.jsoup"               %  "jsoup"                  % "1.22.1",
     "uk.gov.hmrc"             %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalacheck"          %% "scalacheck"             % "1.19.0",
-    "org.scalamock"           %% "scalamock"              % "7.5.0",
+    "org.scalamock"           %% "scalamock"              % "7.5.5",
     "org.scalatest"           %% "scalatest"              % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-17"        % "3.2.18.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"     % "7.0.2"
